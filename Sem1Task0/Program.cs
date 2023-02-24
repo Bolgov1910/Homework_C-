de473f802deef9bdce72ea436e99ecef2a1f2eae
Сергей <bolgov1910@gmail.com>
2023-02-24 21:52:40 +0400

    2задачи

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 0 - Квадрат числа
+Console.WriteLine ("Введите число");
+string? inputNum = Console.ReadLine(); //"?" - не проверяем на Null
+// Проверяем, чтобы данные были не пустые (Null)
+if (inputNum !=null)
+{
+    //int number = int.Parse(inputNum);
+    //int outNum = (int)Math.Pow(number,2);
+    //Console.WriteLine ("Квадрат числа:"+outNum);
+    Console.WriteLine ("Квадрат числа: "+Math.Pow(int.Parse(inputNum),2));
+}
---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 01: Првоерка - являетня ли первое число квадратом второго
+//  
+Console.WriteLine ("Введите первое число");
+string inputNum1 = Console.ReadLine();
+Console.WriteLine ("Введите второе число");
+string inputNum2 = Console.ReadLine();
+if (inputNum1 != null && inputNum1 != null)
+{
+    int number1 = int.Parse(inputNum1);
+    int number2 = int.Parse(inputNum2);
+    if (number1 == (int)Math.Pow(number2,2))
+    {
+        Console.WriteLine ("Да - Первое число является квадратом второго");
+    }
+    else
+    {
+        Console.WriteLine ("Нет - Первое число не является квадратом второго");
+    }
+
+}
