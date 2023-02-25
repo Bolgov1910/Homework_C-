5e8d05b28bc293684c2cbad10f545a4fab3bdacd
Сергей <bolgov1910@gmail.com>
2023-02-25 10:39:07 +0400

    Задача3_второй нет, была заведена ошибочно

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 03: Вывод дня недели  
+//  
+Console.WriteLine ("Введите номер дня недели");
+string? NumDay = Console.ReadLine();
+string[] DayWeek = {"Пон", "Вт", "Ср","Чт","Птн","Сбб","Вскр"};
+int Day = int.Parse(NumDay);
+if (NumDay!=null)
+{
+    if (Day<8 && Day>0)
+    Console.WriteLine (DayWeek[Day - 1]);
+    else
+    {
+        Console.WriteLine ("Некорректный номер дня недели");
+    }
+
+}
