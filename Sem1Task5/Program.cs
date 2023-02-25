bee04b8439a92c70f88697ce1651eecd25befbbc
Сергей <bolgov1910@gmail.com>
2023-02-25 13:19:37 +0400

    Задача 5

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Все целые числа от –n до n
+Console.WriteLine("Вести число:");
+string? num = Console.ReadLine();
+if (num!=null)
+{
+    int namStart = int.Parse(num);
+    int namJob = namStart*(-1);
+    string outLine = string.Empty;
+    while (namJob<namStart)
+    {
+        outLine = outLine + namJob + ",   ";
+        namJob++; // namJob = namJob + 1
+    }
+    outLine = outLine + namStart;
+    Console.WriteLine(outLine);
+}
