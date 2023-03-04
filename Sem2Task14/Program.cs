c60a888bdddd6aac960f220fce6d909fb142ce26
Сергей <bolgov1910@gmail.com>
2023-03-04 12:10:55 +0400

    Семинар задача14 - 3 способа

--- Sem2Task14/Program.cs
+++ Sem2Task14/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine ("Введите первое число");
+﻿Console.WriteLine ("Введите число");
 int nam = int.Parse(Console.ReadLine()??"0");
 // проверка сразу 2-х условий
 if ((nam%7==0) && (nam%23==0)) 
@@ -10,8 +10,17 @@ else
     Console.WriteLine("Некратно 7 и 23");
 }
 
-Console.WriteLine ("Введите первое число для использования bool");
+// операция как переменная
+Console.WriteLine ("Введите число для использования bool");
 int namA = int.Parse(Console.ReadLine()??"0");
 // проверка сразу 2-х условий
 bool oper = ((nam%7==0) && (nam%23==0));
 Console.WriteLine("Некратно  " + oper);
+
+
+// с тернарным оператором
+Console.WriteLine ("Введите число для тернарного опенратора");
+int namB = int.Parse(Console.ReadLine()??"0");
+// проверка сразу 2-х условий
+string rez = ((nam%7==0) && (nam%23==0))? "Кратно(терн.оп)" : "Некратно(терн.оп)";
+Console.WriteLine(rez);
