25f1e4c95a6a01c4b22a8e11b38e6117fcfe1be4
Сергей <bolgov1910@gmail.com>
2023-03-04 11:45:29 +0400

    Семинар задача14

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿// второе число кратно первому, или выводится остаток
+
+// вводим 1 число с консоли c проверкой: если введено некорректное значение, выдается 0.
+int nam1 = int.Parse(Console.ReadLine()??"0");
+// вводим 2 число с консоли другим методом – он медленнее (внутри больше проверок)
+// int nam2 = Convert.Toint()(Console.ReadLine());
+// Не получилось - " error CS0117: "Convert" не содержит определение для "Toint"."
+// Если ....ToInt32.... - воообще вылетает
+int nam2 = int.Parse(Console.ReadLine()??"0");
+int rem = nam2% nam1;
+if (rem ==0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Некратно. Остаток:  " + rem);
+}
---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите первое число");
+int nam = int.Parse(Console.ReadLine()??"0");
+// проверка сразу 2-х условий
+if ((nam%7==0) && (nam%23==0)) 
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Некратно 7 и 23");
+}
+
+Console.WriteLine ("Введите первое число для использования bool");
+int namA = int.Parse(Console.ReadLine()??"0");
+// проверка сразу 2-х условий
+bool oper = ((nam%7==0) && (nam%23==0));
+Console.WriteLine("Некратно  " + oper);
