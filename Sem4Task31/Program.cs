7ff744f87666d9acd5dc47d9f2b2b443165477ef
Сергей <bolgov1910@gmail.com>
2023-03-16 23:46:13 +0400

    Задача 32

---
+++ Sem4Task31/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задача №31 Задайте массив из 12 элементов, заполненный случайными числами 
+// из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+
+
+
+// меnод вывода данных - результат
+void PrinData (string res, int value)
+{
+    Console.WriteLine (res + value);
+}
+
+// метод заполнения - универсальный
+int[] Gen1DArr(int len, int min, int max) // длина массива, минимум и максимум случайных чисел
+{
+    if (min>max)  // числа переставляем, если введены неправильно
+    {
+        int buff = min;
+        min = max;
+        max = buff; 
+    }
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for(int i=0; i<len; i++) 
+    {
+        arr[i] =  rnd.Next(min,max+1); // +1 - последнее число в интервал не входит
+    }
+    return arr;
+}
+
+// складываем положительные (позитив) и отрицательные (негатив)
+(int posit, int negot) NegPosSumm(int[] arr)  // метод сразу с двуми результатами
+{
+    int positSum =0;
+    int negotSum =0;
+    for (int i=0; i < arr.Length; i++)
+    {
+        if (arr[i]>0)
+        {
+            positSum = positSum + arr[i];
+        }
+        else
+        {
+            negotSum = negotSum + arr[i];
+        }
+    }
+
+    return (positSum, negotSum);
+}
+
+// Метод печати -  1 одномерный массив
+void Print1DArr (int[] arr) 
+{     
+   Console.Write("[");                               //кавчка спереди 
+    for (int i = 0; i < arr.Length - 1; i++)        // элементы массива (-1) - без последнего элемента 
+    {         
+        Console.Write($"{arr[i]}, ");                // на вывод
+    }     
+    Console.WriteLine($"{arr[arr.Length - 1]}]");    // вывод последнего элемента - это чтобы не было запятой в конце
+}  
+
+int[] testArr = Gen1DArr (15, -9, 9);                      // генерируем массив 
+Print1DArr (testArr);                                   // печатаем массив
+// создаем пару переменных под одним именем и заполняем их методом (нн тоже выдает пару результатов)
+(int posit, int negot) results = NegPosSumm(testArr);  
+PrinData ("Сумма положительных:", results.posit);   // печать переменной: сначала общее имя, потом навание
+PrinData ("Сумма ортрицательных:", results.negot);  // печать переменной: сначала общее имя, потом навание
---
+++ Sem4Task32/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №32 Напишите программу замена элементов массива: положительные 
+// элементы замените на соответствующие отрицательные, и наоборот.
+
+
+
+// метод заполнения - универсальный
+int[] Gen1DArr(int len, int min, int max) // длина массива, минимум и максимум случайных чисел
+{
+    int[] arr = new int[len];
+    for(int i=0; i<len; i++) arr[i] =  new Random().Next(min,max+1); // +1 - последнее число в интервал не входит
+    return arr;
+}
+
+// метод: печать
+void Print1DArr(int[] arr) 
+{     
+    Console.Write("[");                               //кавчка спереди 
+    for (int i = 0; i < arr.Length - 1; i++)        // элементы массива (-1) - без последнего элемента 
+    {         
+        Console.Write($"{arr[i]}, ");                // на вывод
+    }     
+    Console.WriteLine($"{arr[arr.Length - 1]}]");    // вывод последнего элемента - это чтобы не было запятой в конце
+}  
+
+// Делаем иневерсию
+void InversArr (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i]*(-1);
+    }
+}
+int[] test = Gen1DArr (15,-9,9);
+Print1DArr(test);    // Чтобы посмотреть заполнение:
+InversArr (test);  // инверсия
+Print1DArr(test);   // печать с обратным знаком
