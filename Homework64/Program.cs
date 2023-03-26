05a70bf5b7cb93b8bddb69adfb6bd15f16bce3f4
Сергей <bolgov1910@gmail.com>
2023-03-26 18:18:19 +0400

    Задача 68

---
+++ Homework64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от 1 до N. Выполнить с помощью рекурсии.
+// N = 5 -> "1, 2, 3, 3, 4"
+// N = 8 -> "1, 2, 3, 4, 5, 6, 7, 8"
+
+
+Console.WriteLine ("Введите число N:");
+string? Num = Console.ReadLine();
+int number = int.Parse(Num);
+
+// Метод вывода чисет
+int NaturalNumber(int n, int m)  // m - начало диапазона чисел, ряд будет от m до n
+{
+    if (n == m)      // до тех пор, пока m не сравняется с n
+    {
+        return n;
+    }
+    else Console.Write($"{NaturalNumber(n, m + 1)}, "); // печать чисел: рекурсия
+  return m;
+}
+
+if (number < 1)        // проверка. Число д.б. > 0, иначе рекурсия не остановится
+{
+  Console.WriteLine("Ввдите число больше 0!");
+  return;
+}
+Console.WriteLine(NaturalNumber(number, 1)); // 
+
---
+++ Homework66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine ("Введите число M");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число N");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+// Метод расчета суммы
+int SumNum(int n, int m)
+{
+  if (n == m) return n;
+  else return SumNum(n + 1, m) + n; // рекурсия
+}
+
+Console.WriteLine($"Cумма натуральных элементов в промежутке от {Num1} до {Num2} равна, {SumNum(Num1, Num2)}");
+
